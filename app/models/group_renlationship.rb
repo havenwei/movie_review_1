f358c548755f116468cd6fd11542dc6ea6e03646
havenwei <havenwei82@163.com>
2017-03-22 14:54:02 +0800

    implement group relation

---
+++ app/models/group_renlationship.rb
@@ -0,0 +1,4 @@
+class GroupRenlationship < ApplicationRecord
+  belongs_to :group
+  belongs_to :user
+end
---
+++ test/models/group_renlationship_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GroupRenlationshipTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
