5ecb506c597e75e2e34060b6e1521ac436dc5d0b
havenwei <havenwei82@163.com>
2017-03-24 01:40:34 +0800

    add all

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,7 @@ class Group < ApplicationRecord
 
   has_many :group_relationships
   has_many :members, through: :group_relationships, source: :user
+
+  has_attached_file :image, styles: {large: "600x600>", medium: "300x300>", thumb: "150x150#"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322070544) do
+ActiveRecord::Schema.define(version: 20170323170004) do
 
   create_table "group_relationships", force: :cascade do |t|
     t.integer  "group_id"
@@ -29,17 +29,25 @@ ActiveRecord::Schema.define(version: 20170322070544) do
   create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.integer  "group_id"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
